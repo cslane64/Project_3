a4d402ea06d24c34e894868990da74acd82bad97
cslane64 <cslane@gmail.com>
2019-08-03 08:56:00 -0700

    a little bit closer

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,6 +2,10 @@ import React, { Component} from 'react';
 import logo from "../img/Logo50th.jpg";
 import { Link } from "react-router-dom";
 import Header from "./Header"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 
 //import test from "./util";
@@ -36,16 +40,24 @@ class LogIn extends Component {
         <Header/>
         <img src={ logo } alt="logo" />
 
-        <form onSubmit={ (e)=> {e.preventDefault();this.props.handleLogIn(this.state.username, this.state.password)}}>
-          <label>Email Address:<input type="text" name="name" onChange={ this.handleNameChange}/></label>
-          <br />
-          <br />
-          <label>Password:<input type="text" name="password" onChange= {this.handlePasswordChange}/></label>
-          <br />
-          <br />
-          <input type="submit" value="Log In"  />  
-             
-        </form>
+        <Container>
+          <Row>
+            <Col></Col>
+            <Col>
+              <form onSubmit={ (e)=> {e.preventDefault();this.props.handleLogIn(this.state.username, this.state.password)}}>
+                <label>Email Address:<input type="text" name="name" onChange={ this.handleNameChange}/></label>
+                <br />
+                <br />
+                <label>Password:<input type="text" name="password" onChange= {this.handlePasswordChange}/></label>
+                <br />
+                <br />
+                <input type="submit" value="Log In"  />  
+              </form>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+        
 
         <Link to={'/register'}>Register</Link> 
       </div>
