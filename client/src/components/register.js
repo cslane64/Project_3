c6519e5aef97135b61ec91716c575ad210219e23
cslane64 <cslane@gmail.com>
2019-08-08 14:55:11 -0400

    ready to give up...

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -46,7 +46,7 @@ class Register extends Component {
       
         
     })
-    this.props.history.push('/api/contacts');
+    this.props.history.push('/');
     console.log(this.state);
 
   };
@@ -81,10 +81,64 @@ class Register extends Component {
                       <Form.Label>Password</Form.Label>
                       <Form.Control  name="password" type="password" placeholder="Password" onChange={e => this.change(e)} />
                     </Form.Group>
-
+                                        
                     <Form.Group controlId="formBasicLocality">
                       <Form.Label>State</Form.Label>
-                      <Form.Control  name="locality" type="text" placeholder="State" onChange={e => this.change(e)} />
+                      <Form.Control name="locality" type='text' as="select" onChange={e => this.change(e)}>
+                      <option>Select your State</option>
+                      <option>Alabama</option>
+                      <option>Alaska</option>
+                      <option>Arizona</option>
+                      <option>Arkansas</option>
+                      <option>California</option>
+                      <option>Colorado</option>
+                      <option>Connecticut</option>
+                      <option>Delaware</option>
+                      <option>District of Columbia</option>
+                      <option>Florida</option>
+                      <option>Georgia</option>
+                      <option>Hawaii</option>
+                      <option>Idaho</option>
+                      <option>Illinois</option>
+                      <option>Indiana</option>
+                      <option>Iowa</option>
+                      <option>Kansas</option>
+                      <option>Kentucky</option>
+                      <option>Louisiana</option>
+                      <option>Maine</option>
+                      <option>Maryland</option>
+                      <option>Massachusetts</option>
+                      <option>Michigan</option>
+                      <option>Minnesota</option>
+                      <option>Mississippi</option>
+                      <option>Missouri</option>
+                      <option>Montana</option>
+                      <option>Nebraska</option>
+                      <option>Nevada</option>
+                      <option>New Hampshire</option>
+                      <option>New Jersey</option>
+                      <option>New Mexico</option>
+                      <option>New York</option>
+                      <option>North Carolina</option>
+                      <option>North Dakota</option>
+                      <option>Ohio</option>
+                      <option>Oklahoma</option>
+                      <option>Oregon</option>
+                      <option>Pennsylvania</option>
+                      <option>Rhode Island</option>
+                      <option>South Carolina</option>
+                      <option>South Dakota</option>
+                      <option>Tennessee</option>
+                      <option>Texas</option>
+                      <option>Utah</option>
+                      <option>Vermont</option>
+                      <option>Virginia</option>
+                      <option>Washington</option>
+                      <option>West Virginia</option>
+                      <option>Wisconsin</option>
+                      <option>Wyoming</option>
+                    </Form.Control>
+                      {/* <Form.Control  name="locality" type="text" placeholder="State" onChange={e => this.change(e)} /> */}
                     </Form.Group>
                     
                     <Button variant="primary" type="submit">
