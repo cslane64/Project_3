3620d3d81e30915cd203951f7ed7bef6df4e9778
cslane64 <cslane@gmail.com>
2019-08-10 15:59:16 -0400

    changed back sequelize sync to server.js

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use(function (req, res) {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-db.sequelize.sync().then(function(){
-    app.listen(PORT, function() {
-        console.log("App now listening on port:  ", PORT);
-    });
-});
-//app.listen(PORT, () => console.log(`Listening on port %s`, PORT));
+// db.sequelize.sync().then(function(){
+//     app.listen(PORT, function() {
+//         console.log("App now listening on port:  ", PORT);
+//     });
+// });
+app.listen(PORT, () => console.log(`Listening on port %s`, PORT));
