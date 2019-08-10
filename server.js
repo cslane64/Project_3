b45349b29154fe440fed316939ec66b79c42badd
cslane64 <cslane@gmail.com>
2019-08-10 16:07:18 -0400

    changed back sequelize sync to server.js 3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 
 
-const api_routes = require ("./routes/api_routes");
+const api_routes = require ("./routes/api_routes"); 
 const auth_routes = require ("./routes/auth_routes");
 
 if (process.env.NODE_ENV === "production") {
