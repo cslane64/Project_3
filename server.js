0a39bc78750eee67ee8d7373d69fc81c4dec3488
cslane64 <cslane@gmail.com>
2019-08-10 11:32:34 -0400

    pre-heroku push

--- server.js
+++ server.js
@@ -5,22 +5,22 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 const api_routes = require ("./routes/api_routes");
 const auth_routes = require ("./routes/auth_routes");
 
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(client/build));
+}
 
 app.use("/", api_routes);
 
-
+app.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
-
-    app.listen(PORT, () => console.log(`Listening on port %s`, PORT));
+app.listen(PORT, () => console.log(`Listening on port %s`, PORT));
